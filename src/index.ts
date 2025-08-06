a80ae7e197f3e3102cca9dbff34479aa7382c01e
Atiwat Seenark <um_oom@hotmail.com>
2025-08-06 21:28:22 +0700

    change to port 6000

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia().get("/",
-  () => "Hello Elysia").listen(4321);
+  () => "Hello Elysia").listen(6000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
